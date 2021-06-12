78bf47c6c3f3803a248a55e7f00fc2706cab2ded
@st80ene <etienejames5@gmail.com>
2021-06-12 07:30:05 +0100

    solved getArea challenge

---
+++ HackerRank/10 Days Of JavaScript/arithmeticOperation.js
@@ -0,0 +1,23 @@
+/**
+ * Question:
+ * Complete the following functions in the editor below:
+ * 1. getArea(length, width): Calculate and return the
+ * area of a rectangle having sides length and width.
+ * 2.getPerimeter(length, width): Calculate and return
+ * the perimeter of a rectangle having sides length and width.
+ * The values returned by these functions are printed to
+ * stdout by locked stub code in the editor.
+ */
+
+//Solution:
+
+function getArea(length, width) {
+	let area;
+	// Write your code here
+	area = length * width;
+	return area;
+}
+
+//testing the solution
+
+console.log(getArea(5, 4));
